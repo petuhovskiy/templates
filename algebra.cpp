13cce88f80bd969347dd29de3f72d8b4151c727b
vladmosko <z38@mosko.ru>
2016-06-05 20:48:19 +0300

    Added algebra

--- algebra.cpp
+++ algebra.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 namespace Algebra {
 
-	/// Returns the amount of numbers comprime with n from range 1..n
+	/// Returns the amount of numbers coprime with n from range 1..n
 	/// O(sqrt(N))
 	template<typename T> T phi(T n) {
 		T r = n;
@@ -22,10 +22,10 @@ namespace Algebra {
 		return r;
 	}
 
-	/// Returns the amount of numbers comprime with n from range 1..n
+	/// Returns the amount of numbers coprime with n from range 1..n
 	/// O(NlogN) for preprocessing, O(1) for a single call
 	/// Not recommended to use with limit more than 10^6, n should be up to limit
-	/// If you call this many times, keep limit non-decreasing
+	/// If you call this many times, keep limit non-increasing
 	int numerous_phi(int limit, int n) {
 		static vector<int> phi;
 		static bool called = false;
@@ -93,19 +93,15 @@ namespace Algebra {
 	/// O(NlogN) for each call
 	vector<int> primes_up_to(int r) {
 		vector<bool> sieve;
-		sieve.resize(r + 1);
+		sieve.assign(r + 1, false);
+		vector<int> res;
 		for (int i = 2; i <= r; i++) {
 			if (sieve[i]) {
 				continue;
 			}
+			res.push_back(i);
 			for (int j = i; j <= r; j += i) {
 				sieve[j] = true;
-			}
-		}
-		vector<int> res;
-		for (int i = 2; i <= r; i++) {
-			if (!sieve[i]) {
-				res.push_back(i);
 			}
 		}
 		return res;
@@ -127,9 +123,10 @@ namespace Algebra {
 		return d;
 	}
 
-	/// Calculate n'th Fibonacci number modulo mod
+	/// Calculate n'th Fibonacci number
 	/// Probably O(logN), though works incredibly fast
-	void calculate_fibonacci(long long n, long long &x, long long &y, long long mod) {
+	/// Result stored in x
+	void calculate_fibonacci(long long n, long long &x, long long &y) {
 		if (n == 0) {
 			x = 0;
 			y = 1;
@@ -138,10 +135,31 @@ namespace Algebra {
 
 		if (n & 1) {
 			calculate_fibonacci(n - 1, y, x);
-			y = (y + x) % mod;
+			y = (y + x);
 		} else {
 			long long a, b;
 			calculate_fibonacci(n >> 1, a, b);
+			y = (a * a + b * b);
+			x = (a * b + a * (b - a));
+		}
+	}
+
+	/// Calculate n'th Fibonacci number modulo mod
+	/// Probably O(logN), though works incredibly fast
+	/// Result stored in x
+	void calculate_fibonacci(long long n, long long &x, long long &y, long long mod) {
+		if (n == 0) {
+			x = 0;
+			y = 1;
+			return;
+		}
+
+		if (n & 1) {
+			calculate_fibonacci(n - 1, y, x, mod);
+			y = (y + x) % mod;
+		} else {
+			long long a, b;
+			calculate_fibonacci(n >> 1, a, b, mod);
 			y = (a * a + b * b) % mod;
 			x = (a * b + a * (b - a + mod)) % mod;
 		}
@@ -181,7 +199,7 @@ namespace Algebra {
 	/// Returns least prime divisor of n
 	/// O(NlogN) for preprocessing, O(1) for a single call
 	/// Not recommended to use with limit more than 10^6, n should be up to limit
-	/// If you call this many times, keep limit non-decreasing
+	/// If you call this many times, keep limit non-increasing
 	int least_prime_divisor(int limit, int n) {
 		static vector<int> lp;
 		static bool called = false;
@@ -207,6 +225,73 @@ namespace Algebra {
 };
 
 int main() {
+
+	// Testing & usage example of phi
+	assert(Algebra::phi(1) == 1);
+	assert(Algebra::phi(2) == 1);
+	assert(Algebra::phi(3) == 2);
+	assert(Algebra::phi(4) == 2);
+	assert(Algebra::phi(5) == 4);
+
+	// Testing & usage example of numerous_phi
+	assert(Algebra::numerous_phi(5, 5) == 4);
+	assert(Algebra::numerous_phi(5, 4) == 2);
+	assert(Algebra::numerous_phi(5, 3) == 2);
+	assert(Algebra::numerous_phi(5, 2) == 1);
+	assert(Algebra::numerous_phi(5, 1) == 1);
+
+	// Testing & usage example of binpow
+	assert(Algebra::binpow(2, 5) == 32);
+	assert(Algebra::binpow(3, 11) == 177147);
+	assert(Algebra::binpow(5, 7) == 78125);
+
+	// Testing & usage example of binpow with modulo
+	assert(Algebra::binpow(2, 5, 31) == 1);
+	assert(Algebra::binpow(3, 11, 177147) == 0);
+	assert(Algebra::binpow(2, 12344, 2) == 0);
+
+	// Testing & usage example of gcd
+	assert(Algebra::gcd(2, 6) == 2);
+	assert(Algebra::gcd(33, 11) == 11);
+	assert(Algebra::gcd(354, 2) == 2);
+	assert(Algebra::gcd(0, 5) == 5);
+
+	// Testing & usage example of primes_up_to
+	vector<int> a = Algebra::primes_up_to(10);
+	assert(a.size() == 4);
+	assert(a[0] == 2 && a[1] == 3 && a[2] == 5 && a[3] == 7);
+
+	// Testing & usage example of extended_gcd
+	int x, y;
+	assert(Algebra::extended_gcd(12, 30, x, y) == 6);
+	assert(x * 12 + y * 30 == 6);
+
+	// Testing & usage example of calculate_fibonacci
+	long long u, v;
+	Algebra::calculate_fibonacci(5, u, v);
+	assert(u == 5);
+
+	// Testing & usage example of calculate_fibonacci with modulo
+	Algebra::calculate_fibonacci(5, u, v, 3);
+	assert(u == 2);
+
+	// Testing & usage example of gray_code
+	assert(Algebra::gray_code(5) == 7);
+	assert(Algebra::gray_code(1) == 1);
+
+	// Testing & usage example of inverse_gray_code
+	assert(Algebra::inverse_gray_code(7) == 5);
+	assert(Algebra::inverse_gray_code(1) == 1);
+
+	// Testing & usage example of diofantite2_any_solution
+	long long q, b, c, d, e, f;
+	assert(Algebra::diofantite2_any_solution(q, b, c, d, e, f));
+	assert(d * q + e * b == c);
+
+	// Testing & usage example of least_prime_divisor
+	assert(Algebra::least_prime_divisor(10, 2) == 2);
+	assert(Algebra::least_prime_divisor(10, 6) == 2);
+	assert(Algebra::least_prime_divisor(10, 7) == 7);
 
 	return 0;
 }
